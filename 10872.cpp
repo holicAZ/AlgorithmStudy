7a3f25936b45b33daa2e6ea9d60776b02feece53
holicAZ <ajm4232@gmali.com>
2020-07-22 16:45:36 +0900

    팩토리얼(재귀)

---
+++ 10872.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	if (n <= 1) return 1;
+
+	return n * factorial(n - 1);
+
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int n;
+
+	cin >> n;
+	cout << factorial(n);
+}
