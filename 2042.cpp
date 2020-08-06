08331d8838e88daf30ad42646f77af5e85fb85b0
holicAZ <59554748+holicAZ@users.noreply.github.com>
2020-08-06 23:08:29 +0900

    구간 합 구하기(세그먼트 트리)

--- 2042.cpp
+++ 2042.cpp
@@ -1,38 +1,69 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 using namespace std;
-typedef long long int llt;
 
-int n, m, k;
-llt seg[1 << 21];
+vector <int> v;
+int tree[100000];
 
-llt query(int node, int start, int end, int left, int right) {
-	if (end < left || right < start)return 0;
-	if (left <= start && end <= right) return seg[node];
-
-	int mid = (start + end) >> 1;
-	return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
-
+int init(int start, int end, int node) {
+	if (start == end) return tree[node] = v[start];
+	int mid = (start + end)/2;
+	tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+	return tree[node];
 }
 
+int sum(int start, int end, int node, int left, int right) // 구간 합 구하기
+{
+	if (left > end || right < start) return 0; // 범위 밖에 있는 경우
+	if (left <= start && end <= right) return tree[node]; // 범위 내에 있는 경우
+
+	// 아닐 경우 두 부분으로 나누어 합을 구한다.
+	// 구간을 더한다.
+	int mid = (start + end) / 2;
+	return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+}
+
+//dif : 수정할 값
+//index : 구간 합을 수정하고자 하는 노드
+void update(int start, int end, int node, int index, int dif) {	// 노드의 수정
+	if (index <= start || index >= end) return; // 범위 밖에 있는 경우
+
+	// 범위 내에 있는 경우
+	tree[node] += dif;
+	if (start == end) return;
+	int mid = (start + end) / 2;
+	update(start, mid, node * 2, index, dif);	// 좌측 자식 노드
+	update(mid + 1, end, node * 2 + 1, index, dif);	// 우측 자식 노드
+}
 int main(void) {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	
-	cin >> n >> m >> k;
-	const int sz = pow(2, (int)log2(n - 1) + 1);
-	for (int i = 0; i < n; i++) cin >> seg[sz + i];
-	for (int i = sz - 1; i > 0; i--) seg[i] = seg[i * 2] + seg[i * 2 + 1];
-	m += k;
-	int a, b, c;
-	while (m--) {
-		cin >> a >> b >> c;
-		if (a == 1) {
-			int diff = c - seg[sz + b - 1];
-			for (int node = b + sz - 1; node > 0; node /= 2) seg[node] += diff;
 
-		}
-		else cout << query(1, 1, sz, b, c) << "\n";
+	int n, m, k;
+
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
 	}
-	return 0;
+	init(0, v.size()-1, 1); // 구간 합 트리 생성
+
+	int a, b, c;
+	for (int i = 0; i < (m + k); i++) {
+		cin >> a;
+
+		if (a == 1) {
+			cin >> b >> c;
+			int diff = c - tree[b-1];
+			tree[b-1] = c;
+			update(0, v.size() - 1, 1, b-1, diff);
+		}
+
+		if (a == 2) {
+			cin >> b >> c;
+			cout << sum(0, v.size() - 1, 1, b-1, c-1) << "\n";
+		}
+	}
 }
